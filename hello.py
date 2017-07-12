33b34d0c16e222fde62dda4c2898e0789683e8b4
Nikolay Mahotkin <makhotkin@selectel.ru>
2017-07-12 17:15:08 +0300

    Initial tensorflow-py2

---
+++ hello.py
@@ -0,0 +1,30 @@
+# ==============================================================================
+
+# Disable linter warnings to maintain consistency with tutorial.
+# pylint: disable=invalid-name
+# pylint: disable=g-bad-import-order
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+import os
+
+import tensorflow as tf
+
+tf.app.flags.DEFINE_integer('training_iteration', 1000,
+                            'Number of training iterations')
+tf.app.flags.DEFINE_integer('model_version', 1, 'Version number of the model')
+tf.app.flags.DEFINE_string('data_dir', '/tmp/mnist/data', 'Data directory')
+tf.app.flags.DEFINE_string('log_dir', 'models/mnist', 'Log directory')
+FLAGS = tf.app.flags.FLAGS
+
+
+def main(_):
+    print('Hello, tensorflow!')
+
+
+if __name__ == '__main__':
+    tf.app.run()
